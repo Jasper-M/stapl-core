0e1bb2eefc5563ed230a5b07b8b0eb64f7ea53fd
Jasper Moeys <jaspermoeys@hotmail.com>
2016-11-08 15:27:17 +0100

    Update PolicyParser

--- main/src/main/scala/stapl/core/parser/PolicyParser.scala
+++ main/src/main/scala/stapl/core/parser/PolicyParser.scala
@@ -29,21 +29,10 @@ class PolicyParser {
   settings.usejavacp.value = true
   settings.nowarnings.value = true
   val interpreter = new IMain(settings)
-  interpreter.beQuietDuring({
-    interpreter.interpret(
-      "import stapl.core.{subject => _, resource => _, action => _, _}\n" +
-      "import stapl.core.templates._")
-  })
   
-  /**
-   * Add another import to the Scala interpreter employed by this parser.
-   */
-  def addImport(i: String) = {
-    interpreter.beQuietDuring({
-      interpreter.interpret(s"import $i")
-    })
-  }
 
+  private object lock
+  
   /**
    * Parse the given policy string and return the resulting policy.
    * 
@@ -51,19 +40,22 @@ class PolicyParser {
    * 			The string containing the STAPL policy. This policy should not
    *    		contain attribute definitions and should just contain the policy
    *      		specification, not an assignment to a val. 
-   *        	For example: policyString = "Policy(...) := ..."
+   *        	For example: policyString == "Policy(...) := ..."
    * 
    * @throws 	RuntimeException	
    * 			When the parser did not success in getting 
    * 			an abstract policy from the given string.
    */
   def parse(policyString: String): AbstractPolicy = {
-    val completePolicy = s"val policy = $policyString"
-    interpreter.beQuietDuring({
-      interpreter.interpret(completePolicy)
-    })
-
-    val somePolicy = interpreter.valueOfTerm("policy")
+    val somePolicy = lock.synchronized {
+      interpreter.beQuietDuring({
+        interpreter.interpret(policyString)
+      })
+  
+      val somePolicy = interpreter.valueOfTerm(interpreter.mostRecentVar)
+      interpreter.reset()
+      somePolicy
+    }
 
     somePolicy match {
       case None => throw new RuntimeException("Could not load policy from given policyString (returned None)")
@@ -79,6 +71,13 @@ class PolicyParser {
    * More details: see parse(policyString: String)
    */
   def parseFile(path: String): AbstractPolicy = {
+    val source = io.Source.fromFile(path)
+    val policyString = source.mkString
+    source.close
+    parse(policyString)
+  }
+  
+  def parseFile(path: java.io.File): AbstractPolicy = {
     val source = io.Source.fromFile(path)
     val policyString = source.mkString
     source.close
